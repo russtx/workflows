9104d02caa59fa7af4149df421732b47bb82813d
Russ berge <russ@toplinemediateam.com>
2017-02-13 19:34:34 -0500

    update gulp with coffee and js files in development

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -8,7 +9,7 @@ var jsSources = [
   'components/scripts/rclick.js',
   'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
-  'components/scripts/tagline.js'
+  'components/scripts/template.js'
 ];
 
 gulp.task('coffee', function() {
@@ -20,6 +21,7 @@ gulp.task('coffee', function() {
 
 gulp.task('js', function() {
   gulp.src(jsSources)
-    .pipe(concat('scripts.js'))
+    .pipe(concat('script.js'))
+    .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
 });
